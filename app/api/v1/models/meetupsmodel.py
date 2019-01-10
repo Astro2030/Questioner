078b22ed8d8ff6321feefc905888e646474c2f30
wycliffe <omariwycliffe5@gmail.com>
2019-01-10 13:01:04 +0300

    [starts #163050644] adds meetup model for create meetup

--- app/api/v1/models/meetupsmodel.py
+++ app/api/v1/models/meetupsmodel.py
@@ -12,3 +12,18 @@ class Meetups():
     def get_all_meetups(self):
         return all_meetups
 
+    def get_one_meetup(self, meetup_id):
+        '''Get one meetup'''
+        meetup_available = [
+            meetup for meetup in all_meetups if meetup['meetup_id'] == meetup_id]
+        return meetup_available
+
+
+    def create_meetup(self, meetup_id, location, topic):
+        meetup = {"meetup_id": meetup_id,
+                  "createdOn": datetime.datetime.now().strftime("%y-%m-%d-%H-%M"),
+                  "location": location,
+                  "topic": topic
+                  }
+        all_meetups.append(meetup)
+        return meetup

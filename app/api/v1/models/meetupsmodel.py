928522a7de9a9e690426d290996c56cd22bf993a
wycliffe <omariwycliffe5@gmail.com>
2019-01-10 08:54:34 +0300

    [starts #run.py] adds the get one meetup record model

--- app/api/v1/models/meetupsmodel.py
+++ app/api/v1/models/meetupsmodel.py
@@ -12,3 +12,17 @@ class Meetups():
     def get_all_meetups(self):
         return all_meetups
 
+    def get_one_meetup(self, meetup_id):
+        '''Get one meetup'''
+        meetup_available = [
+            meetup for meetup in all_meetups if meetup['meetup_id'] == meetup_id]
+        return meetup_available
+
+    def create_rsvp(self, meetup_id, status):
+        rsvp_details = {
+            "id": len(all_rsvps)+1,
+            "createdOn": datetime.datetime.now().strftime("%y-%m-%d-%H-%M"),
+            "meetup": meetup_id,
+            "status": status
+        }
+

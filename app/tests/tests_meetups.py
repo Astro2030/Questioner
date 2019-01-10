03b6a9132d875a4792ac48d53b59281ec86eb691
wycliffe <omariwycliffe5@gmail.com>
2019-01-10 09:05:04 +0300

    [finishes #163050466] updates test coverage

--- app/tests/tests_meetups.py
+++ app/tests/tests_meetups.py
@@ -28,6 +28,12 @@ class MeetupsTestCase(unittest.TestCase):
 
         if  response:
             self.assertEqual(response.status_code, 404)
+
+    def test_get_one_meetup(self):
+        '''Test if the user can get a specific meetup record'''
+        response = self.client.get(
+            'api/v1/meetups/1', content_type="application/json")
+        self.assertEqual(response.status_code, 404)
         
 
 '''Standard unittest runner for executing the test'''

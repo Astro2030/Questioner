c4aaf8411f8ed1cec29a8d4d5fbe5f5a9179816b
wycliffe <omariwycliffe5@gmail.com>
2019-01-10 22:06:46 +0300

    [starts #163128034] initial commit

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
-from app.api.v1.views.meetups import AllMeetupsApi
+from app.api.v1.views.meetups import AllMeetupsApi, SingleMeetupApi
+from app.api.v1.views.questions import AllQuestionsApi
 
 
 # create variable called api_v1 that defines the blueprint and registers it in the application factory
@@ -8,3 +9,5 @@ app_v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api_v1 = Api(app_v1)
 
 api_v1.add_resource(AllMeetupsApi, '/meetups/upcoming')
+api_v1.add_resource(SingleMeetupApi, '/meetups/<int:meetup_id>')
+api_v1.add_resource(AllQuestionsApi, '/questions')

5adc271a6b6a2393e89f24b195b3b977c964cedb
wycliffe <omariwycliffe5@gmail.com>
2019-01-11 00:57:08 +0300

    [starts #163129726] updates meetsup model

--- app/api/v1/models/meetupsmodel.py
+++ app/api/v1/models/meetupsmodel.py
@@ -9,6 +9,15 @@ class Meetups():
     def __init__(self):
         pass
 
+    def create_meetup(self, meetup_id, location, topic):
+        meetup = {"meetup_id": meetup_id,
+                  "createdOn": datetime.datetime.now().strftime("%y-%m-%d-%H-%M"),
+                  "location": location,
+                  "topic": topic
+                  }
+        all_meetups.append(meetup)
+        return meetup
+
     def get_all_meetups(self):
         return all_meetups
 
@@ -18,4 +27,14 @@ class Meetups():
             meetup for meetup in all_meetups if meetup['meetup_id'] == meetup_id]
         return meetup_available
 
+    def create_rsvp(self, meetup_id, status):
+        rsvp_details = {
+            "id": len(all_rsvps)+1,
+            "createdOn": datetime.datetime.now().strftime("%y-%m-%d-%H-%M"),
+            "meetup": meetup_id,
+            "status": status
+        }
 
+        all_rsvps.append(rsvp_details)
+
+        return rsvp_details

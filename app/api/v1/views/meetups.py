92d639249a2696fa5f8780cecb9c9beb4a5fb4c5
wycliffe <omariwycliffe5@gmail.com>
2019-01-10 09:04:16 +0300

    [finishes #163050466] adds get one meetup record

--- app/api/v1/views/meetups.py
+++ app/api/v1/views/meetups.py
@@ -14,3 +14,31 @@ class AllMeetupsApi(Resource):
                             "data": meetups})
         response.status_code = 200
         return response
+
+    def post(self):
+        data = request.get_json()
+
+        if not data:
+            return "Data must be in JSON format", 404
+        try:
+            meetup_id = len(all_meetups) + 1
+            location = data["location"]
+            topic = data["topic"]
+
+            meetup_record = Meetups().create_meetup(meetup_id,
+                                                    location, topic)
+            print(all_meetups)
+            print(meetup_record)
+
+            if meetup_record:
+                response = jsonify({"status": 201,
+                                    "data": meetup_record})
+                response.status_code = 201
+                print(all_meetups)
+                return response
+            else:
+                return 'Could not create meetup', 400
+
+        except Exception as e:
+            return "PLease include all details", 400
+           

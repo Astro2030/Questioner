35a4df624ce7275d7d47a62e93d24e79eb604c69
wycliffe <omariwycliffe5@gmail.com>
2019-01-14 00:30:38 +0300

    [starts #163179704] updates models

---
+++ app/api/v1/models/question_model.py
@@ -0,0 +1,26 @@
+'''This module represents the question entity'''
+from datetime import datetime
+
+from app.api.v1.utils.serializer import serialize
+from app.api.v1.models.meetup_model import MeetupModel
+
+ALL_QUESTIONS = [] # Data store for the questions
+
+class QuestionModel:
+    '''Entity representation for a question'''
+    def __init__(self, Author, meetup, title, body):
+        self.question_id = len(ALL_QUESTIONS) + 1
+        self.created_on = str(datetime.utcnow())
+        self.Author = Author
+        self.meetup = meetup
+        self.title = title
+        self.body = body
+        self.upvotes = 0
+        self.downvotes = 0
+
+    @staticmethod
+    def add_question(question, meetup_id):
+        '''Add a new question to the data store'''
+        ALL_QUESTIONS.append(question)
+        meetup = MeetupModel.get_meetup_by_id(int(meetup_id))
+        meetup["questions"].append(serialize(question))
---
+++ app/api/v1/models/user_model.py
@@ -0,0 +1,53 @@
+'''This module represents a user entity'''
+from datetime import datetime
+from passlib.hash import pbkdf2_sha256 as sha256
+
+from app.api.v1.utils.utility import fetch_item
+from instance.config import Config
+
+USERS = [] # Data store for the users
+SECRET_KEY = Config.SECRET_KEY
+
+class UserModel:
+    '''Entity representation for a user'''
+    def __init__(self, firstname, lastname, othername, email, phone_number, \
+                username, is_admin, password):
+        self.user_id = len(USERS) + 1
+        self.firstname = firstname
+        self.lastname = lastname
+        self.othername = othername
+        self.email = email
+        self.phone_number = phone_number
+        self.username = username
+        self.registered = str(datetime.utcnow())
+        self.is_admin = is_admin
+        self.password = password
+
+    def get_user_id(self):
+        '''Fetch a user id'''
+        return self.user_id
+
+    @staticmethod
+    def generate_password_hash(password):
+        '''Generate the hash of the password'''
+        return sha256.hash(password)
+
+    @staticmethod
+    def verify_password_hash(password, hashed_password):
+        '''Compare the password with its hashed value'''
+        return sha256.verify(password, hashed_password)
+
+    @staticmethod
+    def add_user(user):
+        '''Add a new user to the data store'''
+        USERS.append(user)
+
+    @staticmethod
+    def get_all_users():
+        '''Fetch all users'''
+        return USERS
+
+    @staticmethod
+    def get_user_by_username(username):
+        '''Fetch a user given a username'''
+        return fetch_item(username, 'username', USERS)

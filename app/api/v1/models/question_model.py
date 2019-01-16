9a4ed7b02852312a7c90ab5c7399f654e5350fc4
wycliffe <omariwycliffe5@gmail.com>
2019-01-16 21:11:11 +0300

    [starts #163266937] updates question model to accomodate validations

--- app/api/v1/models/question_model.py
+++ app/api/v1/models/question_model.py
@@ -1,16 +1,17 @@
 '''This module represents the question entity'''
 from datetime import datetime
-from app.api.v1.utils.serializer import serialize
+
+from app.api.v1.utils.utility import Utility
 from app.api.v1.models.meetup_model import MeetupModel
 
-ALL_QUESTIONS = [] # Data store for the questions
+QUESTIONS = [] # Data store for the questions
 
 class QuestionModel:
     '''Entity representation for a question'''
-    def __init__(self, Author, meetup, title, body):
-        self.question_id = len(ALL_QUESTIONS) + 1
+    def __init__(self, created_by, meetup, title, body):
+        self.question_id = len(QUESTIONS) + 1
         self.created_on = str(datetime.utcnow())
-        self.Author = Author
+        self.created_by = created_by
         self.meetup = meetup
         self.title = title
         self.body = body
@@ -20,6 +21,6 @@ class QuestionModel:
     @staticmethod
     def add_question(question, meetup_id):
         '''Add a new question to the data store'''
-        ALL_QUESTIONS.append(question)
+        QUESTIONS.append(question)
         meetup = MeetupModel.get_meetup_by_id(int(meetup_id))
-        meetup["questions"].append(serialize(question))
+        meetup["questions"].append(Utility.serialize(question))

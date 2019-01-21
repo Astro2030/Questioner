2be237db6bc721515d55d7b4148efa26bd11412d
wycliffe <omariwycliffe5@gmail.com>
2019-01-21 09:38:30 +0300

    [starts #163292917]modifies the database

--- app/__init__.py
+++ app/__init__.py
@@ -1,41 +1,19 @@
 '''Application entry module'''
-from flask import Flask, jsonify
+from flask import Flask
 from flask_jwt_extended import JWTManager
-# from 
 
 from app.api.v2 import AUTH_BLUEPRINT, API_BLUEPRINT
-
+from app.api.v2.models.db import close_database, init_db_command
 from instance.config import APP_CONFIG
 
 def create_app(config_name):
     '''Instantiate the Flask application'''
     app = Flask(__name__, instance_relative_config=True)
     jwt = JWTManager(app)
-
-    @app.errorhandler(404)
-    def not_found(e):
-        """custom error handler for 404 Not Found Error"""
-        return jsonify({
-            "error": "Not Found. Please refer to the API documentation"
-        }), 404
-
-    @app.errorhandler(500)
-    def server_error(e):
-        """custom error handler for 500 Internal Server Error"""
-        app.logger.error('An error occured', e)
-        return jsonify({
-            "error": "An error occured. Please try again later"
-        }), 500
-
-    @app.errorhandler(405)
-    def not_implemented(e):
-        """custom error handler for 405 Not Implemented Error"""
-        return jsonify({
-            "error": "Method not implemented. Refer to the API documentation"
-        }), 405
     app.config.from_object(APP_CONFIG["development"])
     app.config.from_pyfile('config.py')
-
+    app.teardown_appcontext(close_database)
+    app.cli.add_command(init_db_command)
     app.register_blueprint(AUTH_BLUEPRINT)
     app.register_blueprint(API_BLUEPRINT)
 

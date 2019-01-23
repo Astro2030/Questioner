f47853174ff3ef6ab6c83f822db0f8a9858eefed
wycliffe <omariwycliffe5@gmail.com>
2019-01-24 02:28:43 +0300

    [finishes 163376157] adds post question endpoint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,7 +4,8 @@ Modularize the app using BluePrints and the API resource
 from flask import Blueprint
 from flask_restful import Api
 
-from app.api.v2.views.meetup_view import MeetupList,UpcomingMeetup
+from app.api.v2.views.meetup_view import MeetupList, Meetup, UpcomingMeetup
+from app.api.v2.views.question_view import Question
 from app.api.v2.views.user_view import UserRegistration, UserLogin
 
 AUTH_BLUEPRINT = Blueprint("auth", __name__, url_prefix='/api/v2/auth')
@@ -17,4 +18,6 @@ AUTH.add_resource(UserRegistration, '/register')
 AUTH.add_resource(UserLogin, '/login')
 
 API.add_resource(MeetupList, '/meetups')
+API.add_resource(Meetup, '/meetups/<meetup_id>')
 API.add_resource(UpcomingMeetup, '/meetups/upcoming/')
+API.add_resource(Question, '/meetups/<int:meetup_id>/questions')
---
+++ app/api/v2/views/question_view.py
@@ -0,0 +1,59 @@
+'''This module represents the question view'''
+from flask import abort,make_response,json
+from json import dumps
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_restful import reqparse, Resource
+
+from app.api.v2.models.meetup_model import MeetupModel
+from app.api.v2.models.user_model import UserModel
+from app.api.v2.models.question_model import QuestionModel
+from app.api.v2.utils.validator import ValidationHandler
+
+class Question(Resource):
+    '''Question requests'''
+    @jwt_required
+    def post(self, meetup_id):
+        '''Create a question record'''
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', required=True, help="title cannot be blank!")
+        parser.add_argument('body', required=True, help="body cannot be blank!")
+        parser.add_argument('created_by', required=True, help="body cannot be blank!")
+        data = parser.parse_args()
+
+        current_user = get_jwt_identity()
+        user = UserModel().get_user_by_username(current_user)
+        if not user:
+            abort(401, 'This action requires loggin in!')
+
+
+        question = {
+            "created_by":user['username'],
+            "meetup_id":meetup_id,
+            "title":data['title'],
+            "body":data['body']
+        }
+           
+        # Validate the title
+        ValidationHandler.validate_meetup_title(data['title'])
+
+        # Validate the body
+        ValidationHandler.validate_meetup_body(data['body'])
+
+        # if meetup_id.isdigit():
+        meetup = MeetupModel().get_meetup_by_id(meetup_id)
+        if meetup == {}:
+            abort(404, "Meetup with id '{}' doesn't exist!".format(meetup_id))
+        QuestionModel().add_question(question)
+        meetup['questions'] += 1
+        response = make_response(json.dumps(meetup), 200)
+        response.headers.set('Content-Type', 'application/json')
+        return response
+
+    def get(self, meetup_id):
+        '''Fetch all questions to a specific meetup'''
+        Question = QuestionModel().get_all_questions_by_meetup_id(meetup_id)
+        if not Question:
+            abort(404, 'No question is available')
+        response = make_response(json.dumps(Question), 200)
+        response.headers.set('Content-Type', 'application/json')
+        return response

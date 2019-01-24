3ffb38bb1cc81244ec83a3aba85dfdbff764a20c
wycliffe <omariwycliffe5@gmail.com>
2019-01-24 07:39:54 +0300

    adds a schema for rsvs

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,29 @@ def create_app(config_name='production'):
     '''Instantiate the Flask application'''
     app = Flask(__name__, instance_relative_config=True)
     jwt = JWTManager(app)
+
+    @app.errorhandler(404)
+    def not_found(e):
+        """custom error handler for 404 Not Found Error"""
+        return jsonify({
+            "error": "Not Found. Please refer to the API documentation"
+        }), 404
+
+    @app.errorhandler(500)
+    def server_error(e):
+        """custom error handler for 500 Internal Server Error"""
+        app.logger.error('An error occured', e)
+        return jsonify({
+            "error": "An error occured. Please try again later"
+        }), 500
+
+    @app.errorhandler(405)
+    def not_implemented(e):
+        """custom error handler for 405 Not Implemented Error"""
+        return jsonify({
+            "error": "Method not implemented. Refer to the API documentation"
+        }), 405
+        
     app.config.from_object(APP_CONFIG[config_name])
     app.config.from_pyfile('config.py')
     reg_app(app)
